f43924a95e87312632f441f55d1ad5bda532298a
saku.isaket <saku.isaket@LAPTOP-S2GMU1FV>
2021-02-24 04:53:19 +0530

    ~____~____~

---
+++ src/SOPStatement.java
@@ -0,0 +1,15 @@
+/**
+ * @author saku.isaket
+ *
+ */
+public class SOPStatement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Ok Fine!");
+
+	}
+
+}
